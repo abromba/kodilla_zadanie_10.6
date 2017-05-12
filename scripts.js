8ccaebe0791b54e8ac2d8b98b8de422df9324f78
Ania <Ania>
2017-05-12 11:36:32 +0200

    appendChild method added

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
-var newItem = document.createElement("li");
-var textnode = document.createTextNode("item ...");
-function mojaFunction() {
-    newItem.appendChild(textnode);
-    var list = document.getElementById("myList");
+var list = document.getElementById("list");
+var add = document.getElementById("addElem");
+
+add.addEventListener("click", function() {
+    var li = document.createElement('li');
+    var text = document.createTextNode('item ' + list.childElementCount);
+    li.appendChild(text);	
     list.appendChild(li);
-}
+});
